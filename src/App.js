765add1bed9f6bce54e05a32c9dc8d7e677a7897
afilonov1 <afilonov1@yandex.ru>
2021-09-06 17:33:39 +0300

    components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from  'react'
+import { Message } from './components/Message';
 
 function App() {
+  const [messageList, setMessageList] = useState([]);
+  useEffect( () => {
+      if (messageList[messageList.length - 1]?.author != "Robot") {
+        setTimeout(
+          () => setMessageList((prevState) => [
+            ...prevState,
+            {author: `Robot`, text: `Hi there. Humanitarian`},
+          ]),
+          1500
+        );
+      }
+    }, [messageList]
+  )
+
+
+  const [textInputed, setTextInputed] = useState("");
+
+  function textHandler(event) {
+    setTextInputed(event.target.value)
+  }
+  function addMessageHandler() {
+    setMessageList((prevState) => [
+        ...prevState,
+        {author: `Human`, text: `${textInputed}`},
+    ]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Message className="react" text="My React APP"/>
-      </header>
+      <div className="messages">
+        {messageList.map((message, i ) => {
+          return <Message key={i} author={message.author} text={message.text} />;
+        })}
+     </div>
+
+      <form className="form" onSubmit={(e) => e.preventDefault()}>
+        <input type="text" value={textInputed} onChange={textHandler} />
+        <button onClick={addMessageHandler}>Add comment</button>
+      </form>
     </div>
-  );
+
+  )
 }
-function Message(props) {
-  return <h1 className={props.className}>{props.text}</h1>;
-}
+
 
 export default App;
